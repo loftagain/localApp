8fd4466423e0c94d92d0f3da1ba50e7100d02b05
diana.vigdorova <a@a.com>
2024-05-28 10:52:20 +0300

    Initial commit

---
+++ src/main/java/com/company/localApp/LocalAppApplication.java
@@ -0,0 +1,13 @@
+package com.company.localApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/localApp/LocalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.localApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
