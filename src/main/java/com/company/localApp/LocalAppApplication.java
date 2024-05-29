48ece379b10ba0678c7842cfb790b0022eae772e
diana.vigdorova <a@a.com>
2024-05-29 12:48:23 +0300

    Client save, retrieve, list

--- src/main/java/com/company/localApp/LocalAppApplication.java
+++ src/main/java/com/company/localApp/LocalAppApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class LocalAppApplication {
 
@@ -16,14 +18,34 @@ public class LocalAppApplication {
 	public CommandLineRunner commandLineRunner(ClientInterface clientInterface){
 		return runner-> {
 			createClient(clientInterface);
+			readClient(clientInterface);
+			queryForClientele(clientInterface);
 		};
 }
 
+	private void queryForClientele(ClientInterface clientInterface) {
+		List<Clientele> theClients=clientInterface.findAll();
+		for(Clientele tempClient:theClients){
+			System.out.println(tempClient);
+		}
+	}
+
+	private void readClient(ClientInterface clientInterface) {
+		System.out.println("Retrieving client with id 1");
+		Clientele tempClient=clientInterface.findById(1);
+		System.out.println("Client information is: "+tempClient);
+	}
+
 	private void createClient(ClientInterface clientInterface) {
 		System.out.println("Creating a news client object...");
-		Clientele tempClient=new Clientele("LVHABA00010000","+371 21234002","Hunter Gather");
+		Clientele tempClient=new Clientele("LVHABA00010003","+371 21234002","Hunter Gather");
+		Clientele tempClient2=new Clientele("LVHABA00010001","+371 21234003","Kate Gunther");
+		Clientele tempClient3=new Clientele("LVHABA00010000","+371 21234003","Rachel Gui");
+
 		System.out.println("Saving the client...");
 		clientInterface.save(tempClient);
+		clientInterface.save(tempClient2);
+		clientInterface.save(tempClient3);
 		System.out.println("Saved client. Generated Id: "+tempClient.getId());
 
 	}
