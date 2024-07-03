f228b6ff70ea366d8e56e25355277694702bdace
loftagain <ppkhloe@gmail.com>
2024-07-03 17:59:45 +0300

    yellow glow when stops

--- src/main/java/com/company/localApp/users/WebController.java
+++ src/main/java/com/company/localApp/users/WebController.java
@@ -5,14 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping
 public class WebController {
 
-    @GetMapping
-    public String welcome(Model model) {
+    @GetMapping("/")
+    public String frontPage(Model model) {
         model.addAttribute("pageTitle", "welcome");
         model.addAttribute("styles", "welcome");
         model.addAttribute("pageContent", "welcome");
         return "layout";
     }
+
+@GetMapping("/welcome")
+public String welcome(Model model) {
+    model.addAttribute("pageTitle", "welcome");
+    model.addAttribute("styles", "welcome");
+    model.addAttribute("pageContent", "welcome");
+    return "welcome";
+}
 }
