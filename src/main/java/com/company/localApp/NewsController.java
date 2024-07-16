4f38841ce94f055e6d25dc00ac05739a41a44f39
loftagain <ppkhloe@gmail.com>
2024-07-16 14:33:40 +0300

    cleaned up white containers

--- src/main/java/com/company/localApp/NewsController.java
+++ src/main/java/com/company/localApp/NewsController.java
@@ -22,6 +22,6 @@ public class NewsController {
         model.addAttribute("pageTitle", "news");
         model.addAttribute("styles", "news");
         model.addAttribute("pageContent", "news");
-        return "layout";
+        return "news";
     }
 }
